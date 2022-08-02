3294359bba59bd79d4894b7386fe01db87a7d9a9
Brunna Sousa <brunnad68@gmail.com>
2022-08-02 20:27:13 -0300

    add tocando o som pelo teclado

--- main.js
+++ main.js
@@ -1,27 +1,33 @@
 function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+    document.querySelector(idElementoAudio).play(); //buscar o elemento e da o plau
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 const listaDeSons = document.querySelectorAll('.audio');
- 
-let contador = 0; 
+  
 
-while(contador < listaDeTeclas.length){
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
 
-    //templade string
-    const idAudio = `#som_${instrumento}`;
-    
-    console.log(idAudio);
+    const idAudio = `#som_${instrumento}`; //templade string
 
-    tecla.onclick = function (){
+    tecla.onclick = function (){ //apertar a tecla
         tocaSom(idAudio);
     };
 
-    contador = contador +1;
+    tecla.onkeydown = function(evento) { //pintar de vermelho = ação
+        console.log(evento.code == 'Space' || evento.code == 'Enter' );
+                
+        if(evento.code === 'Space' || evento.code == 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () { //remover esse vermelho quando clicar
+        tecla.classList.remove('ativa');
+    }
 
     console.log(contador);
 }
